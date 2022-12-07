fb0d7ee2d8732adef9b1bb365cea58e0aeaafac6
maf21 <manuelfsilvcr7@gmail.com>
2022-12-07 16:47:10 -0500

    app base

---
+++ resources/js/components/login/login.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Login() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">Login Component</div>
+
+                        <div className="card-body">I'm an Login component!</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
